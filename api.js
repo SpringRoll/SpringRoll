8e5a61a7bcf63feff41dc257fa5ffc2cf8fcf0b3
Matt Karl <matt@cloudkid.com>
2014-09-10 18:16:45 -0400

    Auto-generated commit

--- api.js
+++ api.js
@@ -1,28 +1,72 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Animator",
+        "Animator.AnimTimeline",
+        "AnimatorTimeline",
         "Application",
+        "AssetManager",
+        "BaseState",
+        "Button",
         "CacheManager",
+        "Captions",
+        "CharacterClip",
+        "CharacterController",
         "CombinedCallback",
+        "CreateJSCutscene",
+        "CreateJSDisplay",
         "DelayedCall",
+        "DragManager",
         "EventDispatcher",
         "FallbackWorker",
-        "FunctionUtils",
+        "FunctionTask",
+        "GenericDisplay",
+        "ListTask",
+        "LoadTask",
         "LoaderQueueItem",
         "MediaLoader",
         "MediaLoaderResult",
         "PageVisibility",
+        "PixiDisplay",
+        "PixiTask",
+        "Positioner",
         "SavedData",
+        "ScreenSettings",
+        "Sound",
+        "SoundContext",
+        "SoundInst",
+        "SoundListTask",
+        "StateEvent",
+        "StateManager",
         "SubWorker",
+        "Task",
+        "TaskEvent",
+        "TaskManager",
+        "TimeUtils",
+        "Translate",
+        "UIElement",
+        "UIElementSettings",
+        "UIScaler",
+        "VOPlayer",
         "Worker"
     ],
     "modules": [
-        "cloudkid"
+        "cloudkid",
+        "cloudkid.createjs",
+        "cloudkid.pixi"
     ],
     "allModules": [
         {
             "displayName": "cloudkid",
             "name": "cloudkid"
+        },
+        {
+            "displayName": "cloudkid.createjs",
+            "name": "cloudkid.createjs"
+        },
+        {
+            "displayName": "cloudkid.pixi",
+            "name": "cloudkid.pixi"
         }
     ]
 } };
