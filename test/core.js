af18dcf8365ac930558c618b0c241ad2572d057d
Andrew Start <andrewstart@cloudkid.com>
2015-01-02 10:47:10 -0500

    Added Enum class to Core module.

--- test/core.js
+++ test/core.js
@@ -14,6 +14,26 @@ test('namespace & include', function(assert){
 	delete window.my;
 });
 
+test('Enum', function(assert){
+
+	expect(5);
+
+	var Enum = springroll.Enum;
+	
+	var myEnum = new Enum("valueOf0",
+						"valueOf1",
+						"valueOf2");
+	var myOtherEnum = new Enum({name: "one", value:"1"},
+								"two",
+								{name:"screwSequentialNumbers", value:42});
+	assert.ok(myEnum.valueOf0, "EnumValue was created");
+	assert.notEqual(myEnum.valueOf0, 0, "EnumValues are not integers");
+	assert.notEqual(myEnum.valueOf1, myOtherEnum.one, "EnumValues with the same integer value are not the same");
+	assert.equal(myEnum.valueOf2.asInt, 2, "EnumValue.asInt is correct");
+	assert.equal(myOtherEnum.screwSequentialNumbers, myOtherEnum.valueFromInt(42),
+				"Enum.valueFromInt() returns the correct value, even when nonsequential.");
+});
+
 test('Application', function(assert){
 
 	expect(5);
