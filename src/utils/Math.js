2c07e0c4afb0cbb53f69d1e29e47a97352b63928
Matte Szklarz <matteszklarz@cloudkid.com>
2015-01-05 11:32:30 -0500

    replaced getRandomInt with .randomInt

--- src/utils/Math.js
+++ src/utils/Math.js
@@ -1,29 +1,50 @@
 /**
- *  @module Core
+ * @module Core
  */
 (function(Math)
 {
 	/**
-	 *  Add methods to Math
-	 *  @class Math
+	 * Add methods to Math
+	 * @class Math
 	 */
 
 	/**
-	 * Return a random int between minimum and maximum values. If a single value is supplied, it will return a number between 0 and the supplied value.
+	 * Return a random int between minimum and maximum values.
+	 * If a single value is supplied, it will return a number between 0 and the supplied value.
+	 * @method randomInt
+	 * @static
+	 * @param {int} min Lowest number. If max is omitted, then this becomes max.
+	 * @param {int} max Highest number.
+	 * @return {int} The random value
+	 */
+	Math.randomInt = function(min, max)
+	{
+		if (max === undefined)
+		{
+			max = min;
+			min = 0;
+		}
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	/**
+	 * Return a random int between minimum and maximum values.
+	 * If a single value is supplied, it will return a number between 0 and the supplied value.
 	 * @method getRandomInt
 	 * @static
+	* @deprecated
 	 * @param {int} min Lowest number. If max is omitted, then this becomes max.
 	 * @param {int} max Highest number.
 	 * @return {int} The random value
 	 */
 	Math.getRandomInt = function(min, max)
 	{
-		/*  OVERRIDE
-		 *  allow single-parameter use, where min is
-		 *  assumed to be 0, and max is the supplied single-parameter
-		 *  i.e. function(max) {
-		 *      return <value between 0 and parameter>
-		 *  }
+		/* OVERRIDE
+		 * allow single-parameter use, where min is
+		 * assumed to be 0, and max is the supplied single-parameter
+		 * i.e. function(max) {
+		 *     return <value between 0 and parameter>
+		 * }
 		 */
 		if (max === undefined)
 		{
