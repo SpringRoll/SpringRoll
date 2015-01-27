e4591b5a7ef9284ae98cd7dd20ce9fc779c2c569
Matt Karl <matt@cloudkid.com>
2015-01-27 18:04:18 -0500

    Auto-generated commit

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "Math",
         "Object",
         "String",
+        "extend",
         "include",
         "namespace",
         "springroll.AbstractDisplay",
