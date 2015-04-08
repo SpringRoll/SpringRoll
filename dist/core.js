66dc09d37df62a33b50cb8f1dc8e0a43ea7599a1
Matte Szklarz <matteszklarz@cloudkid.com>
2015-04-08 13:12:27 -0400

    Enum.next() fix for non-zero initial value Enums
    Also merged the slight differences in the main loop if/else

--- dist/core.js
+++ dist/core.js
@@ -289,11 +289,12 @@
 	 * @param {int} value The integer value of the enum.
 	 * @param {String} toString A string for toString() to return, instead of the name.
 	 */
-	var EnumValue = function(name, value, toString) {
-		
-		if(Debug === undefined)
+	var EnumValue = function(name, value, toString)
+	{
+
+		if (Debug === undefined)
 			Debug = include('springroll.Debug', false);
-		
+
 		/**
 		 * The name of the value, for reflection or logging purposes.
 		 * @property {String} name
@@ -317,13 +318,16 @@
 	 * The integer value of this enum entry.
 	 * @property {int} asInt
 	 */
-	Object.defineProperty(EnumValue.prototype, "asInt", {
-		get: function() {
+	Object.defineProperty(EnumValue.prototype, "asInt",
+	{
+		get: function()
+		{
 			return this._value;
 		}
 	});
 
-	EnumValue.prototype.toString = function() {
+	EnumValue.prototype.toString = function()
+	{
 		return this._toString;
 	};
 
@@ -357,18 +361,19 @@
 	*  'name' and 'value' properties will have the specified
 	*  numeric value.
 	*/
-	var Enum = function() {
+	var Enum = function()
+	{
 		var args = Array.isArray(arguments[0]) ?
 			arguments[0] :
 			Array.prototype.slice.call(arguments);
+
 		/**
 		 * A potentially sparse array of each enum value, stored by integer values.
 		 * @property {Array} _byValue
 		 * @private
 		 */
-		// In EcmaScript 5 specs and browsers that support it you can use the Object.defineProperty
-		// to make it not enumerable set the enumerable property to false
-		Object.defineProperty(this, '_byValue', {
+		Object.defineProperty(this, '_byValue',
+		{
 			enumerable: false,
 			writable: false,
 			value: []
@@ -380,68 +385,66 @@
 		 * @public
 		 * @property {Array} rawEnumValues
 		 */
-		Object.defineProperty(this, 'rawEnumValues', {
+		Object.defineProperty(this, 'rawEnumValues',
+		{
 			enumerable: false,
 			writable: false,
 			value: args
 		});
-		
+
 		var counter = 0;
-		var len = args.length;
 		var item;
-		var i;
 		var value;
 		var name;
 
-		//create each value
-		for (i = 0; i < len; ++i) {
-			if (typeof args[i] == "string") {
+		// Create an EnumValue for each argument provided
+		for (var i = 0, len = args.length; i < len; ++i)
+		{
+			if (typeof args[i] == "string")
+			{
 				name = args[i];
-				if (this[name]) {
-					if (true && Debug) Debug.error("Error creating enum value " + name + ": " + value +
-						" - an enum value already exists with that name.");
-					continue;
-				}
-				item = new EnumValue(name, counter, name);
-				this[item.name] = item;
-				if (this._byValue[counter]) {
-					if (Array.isArray(this._byValue[counter])) {
-						this._byValue[counter].push(item);
-					}
-					else {
-						this._byValue[counter] = [this._byValue[counter], item];
-					}
-				}
-				else {
-					this._byValue[counter] = item;
-				}
 			}
-			else {
+			else
+			{
 				name = args[i].name;
 				value = args[i].value || counter;
-				if (this[name]) {
-					if (true && Debug) Debug.error("Error creating enum value " + name + ": " + value +
-						" - an enum value already exists with that name.");
-					continue;
-				}
-				item = new EnumValue(name, value, args[i].toString || name);
-				this[item.name] = item;
-				if (this._byValue[value]) {
-					if (Array.isArray(this._byValue[value])) {
-						this._byValue[value].push(item);
-					}
-					else {
-						this._byValue[value] = [this._byValue[value], item];
-					}
-				}
-				else {
-					this._byValue[value] = item;
-				}
 				counter = value;
+			}
+
+			// if name already exists in Enum 
+			if (this[name])
+			{
+				if (true && Debug)
+				{
+					Debug.error("Error creating enum value " + name + ": " + value +
+						" - an enum value already exists with that name.");
+				}
+				continue;
+			}
+
+			item = (typeof args[i] == "string") ?
+				new EnumValue(name, counter, name) :
+				new EnumValue(name, value, args[i].toString || name);
+
+			this[item.name] = item;
+			if (this._byValue[counter])
+			{
+				if (Array.isArray(this._byValue[counter]))
+				{
+					this._byValue[counter].push(item);
+				}
+				else
+				{
+					this._byValue[counter] = [this._byValue[counter], item];
+				}
+			}
+			else
+			{
+				this._byValue[counter] = item;
 			}
 			counter++;
 		}
-				
+
 		/**
 		 * The count of values the enum was initialized with.
 		 * @public
@@ -451,9 +454,9 @@
 		{
 			enumerable: false,
 			writable: false,
-			value: counter
+			value: args.length
 		});
-		
+
 		/**
 		 * Retrieves the next EnumValue in the Enum (loops to first value at end).
 		 * @method {EnumValue} input
@@ -463,12 +466,14 @@
 		{
 			enumerable: false,
 			writable: false,
-			value: function(input) // {EnumValue} input
+			// {EnumValue} input
+			value: function(input)
 			{
 				var nextInt = input.asInt + 1;
-				if (nextInt >= counter) {
+				if (nextInt >= counter)
+				{
 					return this.first;
-					}
+				}
 				return this.valueFromInt(nextInt);
 			}
 		});
@@ -482,7 +487,7 @@
 		{
 			enumerable: false,
 			writable: false,
-			value: this.valueFromInt(0)
+			value: this.valueFromInt(args[0].value || 0)
 		});
 	};
 
@@ -493,14 +498,15 @@
 	 * @param {int} input The integer value to get an enum value for.
 	 * @return {EnumValue} The EnumValue that represents the input integer.
 	 */
-	// In EcmaScript 5 specs and browsers that support it you can use the Object.defineProperty
-	// to make it not enumerable set the enumerable property to false
-	Object.defineProperty(Enum.prototype, 'valueFromInt', {
+	Object.defineProperty(Enum.prototype, 'valueFromInt',
+	{
 		enumerable: false,
 		writable: false,
-		value: function(input) {
+		value: function(input)
+		{
 			var rtn = this._byValue[input];
-			if (rtn) {
+			if (rtn)
+			{
 				return Array.isArray(rtn) ? rtn[0] : rtn;
 			}
 			return null;
