f9277510f219049513e56d697160e42957ed96ed
Matt Bargar <mattbargar@cloudkid.com>
2015-05-13 12:20:08 -0400

    Update Application to make sure tickCallback exists before calling it

--- src/Application.js
+++ src/Application.js
@@ -425,7 +425,7 @@
 			}
 			else
 			{
-				if (_tickId == -1)
+				if (_tickId == -1 && _tickCallback)
 				{
 					_tickId = _useRAF ?
 						requestAnimFrame(_tickCallback) :
@@ -629,9 +629,12 @@
 
 		//request the next tick
 		//request the next animation frame
-		_tickId = _useRAF ?
-			requestAnimFrame(_tickCallback) :
-			setTargetedTimeout(_tickCallback, TimeUtils.now() - _lastFrameTime);
+		if (_tickCallback)
+		{
+			_tickId = _useRAF ?
+				requestAnimFrame(_tickCallback) :
+				setTargetedTimeout(_tickCallback, TimeUtils.now() - _lastFrameTime);
+		}
 	};
 
 	/**
