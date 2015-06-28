0b6487d23ef21e7f55ac5e23d9793f495e545fae
andrewstart <andrewstart@cloudkid.com>
2015-06-28 10:31:30 -0400

    Added PIXI text mixins to support easier alignment.

--- src/displays/pixi/mixins/Container.js
+++ src/displays/pixi/mixins/Container.js
@@ -10,10 +10,10 @@
 	*  @class Container
 	*/
 
-	var Point = include("PIXI.Container", false);
-	if(!Point) return;
+	var Container = include("PIXI.Container", false);
+	if(!Container) return;
 
-	var p = Point.prototype;
+	var p = Container.prototype;
 	
 	/**
 	 * Determines if width and height will calculate bounds of all children using getLocalBounds(),
@@ -29,72 +29,64 @@
 	p._width = 0;
 	p._height = 0;
 	
-	Object.defineProperty(PIXI.DisplayObjectContainer.prototype, 'width', {
-	get: function() {
-		if(this.useBoundsForSize)
-			return this.scale.x * this.getLocalBounds().width;
-		else
-			return this.scale.x * this._width;
-	},
-	set: function(value) {
-		
-		if(this.useBoundsForSize)
+	Object.defineProperty(PIXI.DisplayObjectContainer.prototype, 'width',
+	{
+		get: function()
 		{
-			var width = this.getLocalBounds().width;
-
-			if(width !== 0)
+			if(this.useBoundsForSize)
+				return this.scale.x * this.getLocalBounds().width;
+			else
+				return this.scale.x * this._width;
+		},
+		set: function(value)
+		{
+			if(this.useBoundsForSize)
 			{
-				this.scale.x = value / width;
+				var width = this.getLocalBounds().width;
+				if(width !== 0)
+					this.scale.x = value / width;
+				else
+					this.scale.x = 1;
+				this._width = value;
 			}
 			else
 			{
-				this.scale.x = 1;
+				if(this._width === 0)
+					this._width = value / this.scale.x;
+				else
+					this.scale.x = value / this._width;
 			}
-			
-			this._width = value;
 		}
-		else
+	});
+
+	Object.defineProperty(PIXI.DisplayObjectContainer.prototype, 'height',
+	{
+		get: function()
 		{
-			if(this._width === 0)
-				this._width = value / this.scale.x;
+			if(this.useBoundsForSize)
+				return this.scale.y * this.getLocalBounds().height;
 			else
-				this.scale.x = value / this._width;
-		}
-	}
-});
-
-Object.defineProperty(PIXI.DisplayObjectContainer.prototype, 'height', {
-	get: function() {
-		if(this.useBoundsForSize)
-			return this.scale.y * this.getLocalBounds().height;
-		else
-			return this.scale.y * this._height;
-	},
-	set: function(value) {
-
-		if(this.useBoundsForSize)
+				return this.scale.y * this._height;
+		},
+		set: function(value)
 		{
-			var height = this.getLocalBounds().height;
-
-			if(height !== 0)
+			if(this.useBoundsForSize)
 			{
-				this.scale.y = value / height;
+				var height = this.getLocalBounds().height;
+				if(height !== 0)
+					this.scale.y = value / height;
+				else
+					this.scale.y = 1;
+				this._height = value;
 			}
 			else
 			{
-				this.scale.y = 1;
+				if(this._height === 0)
+					this._height = value / this.scale.y;
+				else
+					this.scale.y = value / this._height;
 			}
-
-			this._height = value;
 		}
-		else
-		{
-			if(this._height === 0)
-				this._height = value / this.scale.y;
-			else
-				this.scale.y = value / this._height;
-		}
-	}
-});
+	});
 	
 }());
