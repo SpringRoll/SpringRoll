553d456e3399feb30d53b9510bff41f3eb1e25f8
Andrew Start <andrewstart@cloudkid.com>
2015-08-19 10:28:47 -0400

    Added a RequestUtils mixin to better support .fnt files.

--- dist/core.js
+++ dist/core.js
@@ -378,6 +378,42 @@
 }(Object, {}));
 /**
  * @module Core
+ * @namespace createjs
+ */
+(function(undefined)
+{
+	var RequestUtils = include('createjs.RequestUtils', false);
+	var AbstractLoader = include('createjs.AbstractLoader', false);
+
+	if (!RequestUtils) return;
+	
+	/**
+	 * Mixins for the CreateJS RequestUtils static class
+	 * @class RequestUtils
+	 */
+	
+	var orig_getTypeByExtension = RequestUtils.getTypeByExtension;
+	/**
+	 * Overrides getTypeByExtension to add additional types that we want, like .fnt as XML.
+	 * @param {String} extension The file extension.
+	 * @return {String} The load type.
+	 */
+	RequestUtils.getTypeByExtension = function(extension)
+	{
+		if(extension)
+		{
+			switch(extension.toLowerCase())
+			{
+				case "fnt":
+					return createjs.AbstractLoader.XML;
+			}
+		}
+		return orig_getTypeByExtension(extension);
+	};
+
+}());
+/**
+ * @module Core
  * @namespace window
  */
 (function(String, Object)
