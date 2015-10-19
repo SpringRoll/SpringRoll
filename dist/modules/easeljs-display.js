524ba991e5567ce1fe83836b663498e5c4810452
Andrew Start <andrewstart@cloudkid.com>
2015-10-19 11:30:05 -0400

    Fixed FlashArt loading SpriteSheets.

--- dist/modules/easeljs-display.js
+++ dist/modules/easeljs-display.js
@@ -808,7 +808,12 @@
 					if (!asset.type)
 						asset.type = "easeljs";
 					if (!asset.id)
-						asset.id = "asset_" + (assetCount++);
+					{
+						var src = asset.src;
+						src = src.substr(0, src.lastIndexOf("."));
+						src = src.substr(src.lastIndexOf("/") + 1);
+						asset.id = src;
+					}
 				}
 				//standard images
 				else
