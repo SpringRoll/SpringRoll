0659ff8fd4f430af872bb2883477f73d87d2ca5a
Yan Shcherbakov <yan.shcherbakov@redspace.com>
2017-04-07 12:47:03 -0300

    Reverting removed code for p.fadeOut somehow this was removed despite merging master into the branch.

--- src/media/Sound.js
+++ src/media/Sound.js
@@ -672,6 +672,11 @@
 				//fade the last played instance
 				inst = sound.playing[sound.playing.length - 1];
 			}
+			else if (s.loadState == LoadStates.loading)
+			{
+				this.stop(aliasOrInst);
+				return;
+			}
 		}
 		else
 		{
