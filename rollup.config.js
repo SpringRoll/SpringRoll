1f642f0a1b1e0c5ef3e176d1741cf882708f2ddf
Andrew Smith <andrew.smith@redspace.com>
2018-05-11 09:39:19 -0300

    SR2: Added Uglify and Source Maps

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,38 @@ import commonjs from 'rollup-plugin-commonjs';
 import eslint from 'rollup-plugin-eslint';
 import prettier from 'rollup-plugin-prettier';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/main.js',
     external: ['ms'],
-    output: [{ file: pkg.esm, format: 'es' }],
+    output: [
+      {
+        file: pkg.esm,
+        format: 'es',
+        sourceMap: true
+      }
+    ],
     plugins: [
       eslint(),
       prettier(),
       babel({
         babelrc: true,
         comments: true,
-        runtimeHelpers: true
+        runtimeHelpers: true,
+        sourceMap: true
       }),
       resolve({
         jsnext: true,
         main: true,
         browser: true
       }),
-      commonjs()
+      commonjs(),
+      uglify({
+        sourceMap: true
+      })
     ]
   }
 ];
