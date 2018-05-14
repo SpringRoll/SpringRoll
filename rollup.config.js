daa1c1e9aedef2bc8cffd9210fa5d43490f157f0
Andrew Smith <andrew.smith@redspace.com>
2018-05-14 15:58:52 -0300

    SR2: Removed babel from build process. Added JSDocs

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import eslint from 'rollup-plugin-eslint';
 import prettier from 'rollup-plugin-prettier';
-import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
@@ -20,21 +19,16 @@ export default [
     plugins: [
       eslint(),
       prettier(),
-      babel({
-        babelrc: true,
-        comments: true,
-        runtimeHelpers: true,
-        sourceMap: true
-      }),
       resolve({
+        module: true,
         jsnext: true,
         main: true,
-        browser: true
+        browser: true,
       }),
       commonjs(),
       uglify({
         sourceMap: true
-      })
+      }),
     ]
   }
 ];
