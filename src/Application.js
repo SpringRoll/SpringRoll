b71193fef8a3a2e9bc42bbbc04af4848d8143f20
Chip Bell <cbell@pbs.org>
2018-06-19 13:59:44 -0400

    Comments

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,11 @@
+/**
+ * Main entry point for a game. Provides a single focal point for plugins and functionality to attach.
+ * @class Application
+ */
 export class Application {
+  /**
+   * Creates a new application, setting up plugins along the way
+   */
   constructor() {
     this.listeners = {};
     Application._plugins.forEach(plugin => plugin.setup.call(this));
@@ -8,11 +15,21 @@ export class Application {
     Promise.all(preloads).then(() => this.emit('init'));
   }
 
+  /**
+   * Attaches an listener to events emitted on this class
+   * @param {String} eventName The name of the event to listen for
+   * @param {Function} method The function to call when the event is emitted
+   */
   on(eventName, method) {
     this.listeners[eventName] = this.listeners[eventName] || [];
     this.listeners[eventName].push(method);
   }
 
+  /**
+   * Emits an event on this application, with some data
+   * @param {String} eventName The event to emit
+   * @param {Object} data The data to emit along with the event
+   */
   emit(eventName, data) {
     if(this.listeners[eventName] instanceof Array) {
       this.listeners[eventName].forEach(function(callback) {
@@ -21,6 +38,12 @@ export class Application {
     }
   }
 
+  /**
+   * Converts a callback-based or synchronous function into a promise. This method is used for massaging plugin preload
+   * methods before they are executed.
+   * @param {Function} f A function that takes either a callback, or returns a promise
+   * @return Promise A promise that resolves when the function finishes executing (whether it is asynchronous or not)
+   */
   promisify(f) {
     // if it takes no argument, assume that it's synchronous or returns a Promise
     if(f.length === 0) {
@@ -40,4 +63,8 @@ export class Application {
   }
 }
 
+/**
+ * The list of plugins that are currently registered to run on Applications
+ * @static
+ */
 Application._plugins = [];
