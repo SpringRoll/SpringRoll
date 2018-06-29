7d26bcd88a98f8d57301caa09ba605a298bfe81d
Chip Bell <cbell@pbs.org>
2018-06-29 16:31:29 -0400

    Extracting event emitting into a separate class

--- src/Application.js
+++ src/Application.js
@@ -1,44 +1,21 @@
+import EventEmitter from './EventEmitter';
+
 /**
  * Main entry point for a game. Provides a single focal point for plugins and functionality to attach.
  * @class Application
  */
-export class Application {
+export class Application extends EventEmitter {
   /**
    * Creates a new application, setting up plugins along the way
    */
   constructor() {
-    this.listeners = {};
+    super();
+    
     Application._plugins.forEach(plugin => plugin.setup.call(this));
     
     const preloads = Application._plugins
       .map(plugin => this.promisify(plugin.preload));
     Promise.all(preloads).then(() => this.emit('init'));
-  }
-
-  /**
-   * Attaches an listener to events emitted on this class
-   * @param {String} eventName The name of the event to listen for
-   * @param {Function} method The function to call when the event is emitted
-   */
-  on(eventName, method) {
-    if(!Array.isArray(this.listeners[eventName])) {
-      this.listeners[eventName] = [];
-    }
-
-    this.listeners[eventName].push(method);
-  }
-
-  /**
-   * Emits an event on this application, with some data
-   * @param {String} eventName The event to emit
-   * @param {Object} data The data to emit along with the event
-   */
-  emit(eventName, data) {
-    if(Array.isArray(this.listeners[eventName])) {
-      this.listeners[eventName].forEach(function(callback) {
-        callback(data);
-      });
-    }
   }
 
   /**
