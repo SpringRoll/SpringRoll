bd93a3ce7104703968c47f1da467e38ff9f30326
Chip Bell <cbell@pbs.org>
2018-09-19 13:35:03 -0400

    Recompiling

--- dist/core.js
+++ dist/core.js
@@ -1,4 +1,4 @@
-/*! SpringRoll 1.1.0 */
+/*! SpringRoll 1.1.1 */
 /**
  * @module Core
  * @namespace window
@@ -2461,7 +2461,7 @@
 	 * @static
 	 * @readOnly
 	 */
-	Application.version = "1.1.0";
+	Application.version = "1.1.1";
 
 	// Reference to the prototype
 	var s = EventDispatcher.prototype;
