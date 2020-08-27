3014916156469e2935f0272e639ba7b752490723
Chip Bell <cbell@pbs.org>
2020-08-27 11:26:52 -0400

    Cleaning up this test suite, in prep for improvements

--- src/state/UserData.spec.js
+++ src/state/UserData.spec.js
@@ -1,37 +1,48 @@
 import { UserData } from './UserData';
-import comm from '../communication/BellhopSingleton';
+import container from '../communication/BellhopSingleton';
 
 document.body.addEventListener('message', t => {
   console.log('called =>', t);
 });
 
+const assertThrows = async (F) => {
+  let didThrow = false;
+  try {
+    await F();
+  } catch(e) {
+    didThrow = true;
+  }
+
+  expect(didThrow).to.equal(true);
+}
+
 describe('UserData', () => {
+  beforeEach(() => {
+    container.connected = true;
+  });
+
+  afterEach(() => {
+    container.connected = false;
+  });
+
   describe('read', () => {
-    it('Should reject if Bellhop is not connected', done => {
-      UserData.read('value').catch(() => {
-        done();
-      });
+    it('Should reject if Bellhop is not connected', async () => {
+      container.connected = false;
+      await assertThrows(() => UserData.read('value'));
     });
-    it('Should timeout if no response from Container', done => {
-      comm.connected = true;
-      UserData.read('name').catch(err => {
-        expect(err).to.equal('No Response');
-        done();
-      });
+
+    it('Should timeout if no response from Container', async () => {
+      await assertThrows(() => UserData.read('value'));
     });
   });
+
   describe('write', () => {
-    it('Should reject if Bellhop is not connected', done => {
-      UserData.write('value', { foo: 'bar' }).catch(() => {
-        done();
-      });
+    it('Should reject if Bellhop is not connected', async () => {
+      container.connected = false;
+      await assertThrows(() => UserData.write('value', { foo: 'bar' }));
     });
-    it('Should timeout if no response from Container', done => {
-      comm.connected = true;
-      UserData.write('name', { foo: 'bar' }).catch(err => {
-        expect(err).to.equal('No Response');
-        done();
-      });
+    it('Should timeout if no response from Container', async () => {
+      await assertThrows(() => UserData.write('name', { foo: 'bar' }));
     });
   });
 });
