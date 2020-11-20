c1b378bf2c8ef6d87d2dc94137a0a98533f6f69f
Eric <ericente@gmail.com>
2020-11-20 12:29:18 -0500

    Fix JSDocs for Property

--- src/state/Property.js
+++ src/state/Property.js
@@ -17,20 +17,14 @@ export class Property {
   }
 
   /**
-   * returns the current value of the property
-   * @readonly
-   * @returns {*}
-   * @memberof Property
+   * The current value of the property
+   * Setting this value notifies all listeners of the change.
+   * @type {*}
    */
   get value() {
     return this._value;
   }
 
-  /**
-   * Sets the value of the property and notifies all listeners of the change
-   * @param {*} value the new property value
-   * @memberof Property
-   */
   set value(value) {
     if (this.value === value && !this.alwaysNotify) {
       return;
@@ -62,7 +56,8 @@ export class Property {
 
   /**
    * Whether or not this property has any subscribed listeners
-   * @return {Boolean} True if this property has at least one subscriber
+   * @readonly
+   * @type {Boolean}
    */
   get hasListeners() {
     return this.listeners.length > 0;
